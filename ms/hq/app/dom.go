2b00a654f04ae5a8d2776c5740f82dfecb00bc8c
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2023-02-22 11:29:56 +0700

    Create business model

--- ms/hq/app/dom.go
+++ ms/hq/app/dom.go
@@ -11,49 +11,46 @@ type User struct {
 }
 
 type AssetInfo struct {
-	ID uint `json:"id,omitempty"`
-	// AccountInfo holds the value of the "account_info" field.
-	AccountInfo struct{} `json:"account_info,omitempty"`
-	// InstitutionInfo holds the value of the "institution_info" field.
-	InstitutionInfo struct{} `json:"institution_info,omitempty"`
-	// AssetInfo holds the value of the "asset_info" field.
-	AssetInfo struct{} `json:"asset_info,omitempty"`
-	// SensibleData holds the value of the "sensible_data" field.
-	SensibleData string `json:"sensible_data,omitempty"`
-	// Descriptions holds the value of the "descriptions" field.
-	Descriptions string `json:"descriptions,omitempty"`
+	ID              uint
+	AccountInfo     struct{}
+	InstitutionInfo struct{}
+	AssetInfo       struct{}
+	SensibleData    string
+	Descriptions    string
 }
 
 type BankAccount struct {
-	ID              uint
-	UserID          uint
-	AccountID       string
-	InstitutionInfo struct{} `json:"institution_info,omitempty"`
-	// AccountInfo holds the value of the "account_info" field.
-	AccountInfo struct{} `json:"account_info,omitempty"`
-	// SensibleData holds the value of the "sensible_data" field.
-	SensibleData string `json:"sensible_data,omitempty"`
+	ID          uint
+	UserID      uint
+	AssetInfoID uint
 }
 
 type Car struct {
-	ID     uint
-	UserID uint
-	Make   string
-	Model  string
-	Year   int32
+	ID          uint
+	UserID      uint
+	AssetInfoID uint
 }
 
 type Collectible struct {
 	ID          uint
 	UserID      uint
-	Name        string
-	Description string
+	AssetInfoID uint
 }
 
 type CryptoAccount struct {
-	ID       uint
-	UserID   uint
-	Name     string
-	CoinType string
-	Balance  float64
+	ID          uint
+	UserID      uint
+	AssetInfoID uint
+}
+
+type Loan struct {
+	ID          uint
+	UserID      uint
+	AssetInfoID uint
+}
+
+type PrivateShare struct {
+	ID          uint
+	UserID      uint
+	AssetInfoID uint
 }
