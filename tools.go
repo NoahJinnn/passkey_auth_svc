a1eaf8b6f839a8aeb3c1fbdc5011952115621ef5
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2023-03-27 17:16:39 +0700

    feat(tools.go): Add golangci-lint and remove unused go:generate command

--- tools.go
+++ tools.go
@@ -1,13 +1,12 @@
 //go:build tools
 
-//go:generate sh -c "GOBIN=$PWD/.gobincache go install $(sed -n 's/.*_ \"\\(.*\\)\".*/\\1/p' <$GOFILE)"
 package tools
 
 import (
-	_ "ariga.io/entimport/cmd/entimport"
 	_ "entgo.io/ent/cmd/ent"
 
 	_ "github.com/cosmtrek/air"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/mattn/goveralls"
 	_ "golang.org/x/tools/cmd/stringer"
 	_ "gotest.tools/gotestsum"
