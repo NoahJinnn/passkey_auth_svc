8fbb0d929cf3705e8f734bee06283054a8233330
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2023-05-09 11:33:35 +0700

    refactor(auth/handlers): Refactor returned errors

--- ms/auth/srv/http/handlers/user.go
+++ ms/auth/srv/http/handlers/user.go
@@ -118,7 +118,7 @@ func (h *UserHandler) Logout(c echo.Context) error {
 
 	cookie, err := h.sessionManager.DeleteCookie()
 	if err != nil {
-		return fmt.Errorf("failed to create session token: %w", err)
+		return fmt.Errorf("failed to delete session token: %w", err)
 	}
 
 	c.SetCookie(cookie)
