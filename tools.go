2dbaf09e623e094bf4fb54c508486aa78e8663df
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2023-05-25 16:58:42 +0700

    ci(CI&CD.yml): Update github action script

--- tools.go
+++ tools.go
@@ -6,7 +6,6 @@ import (
 	_ "entgo.io/ent/cmd/ent"
 
 	_ "github.com/cosmtrek/air"
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/mattn/goveralls"
 	_ "golang.org/x/tools/cmd/stringer"
 	_ "gotest.tools/gotestsum"
