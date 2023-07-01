753edd977f82f0f324cf0f46ac5ec9ab8aea0600
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2023-07-01 15:53:57 +0700

    refactor(scripts/sql): Remove old sql files

--- scripts/sql/auth/create.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    first_name TEXT NOT NULL,
-    last_name TEXT NOT NULL,
-    fido2_key TEXT NOT NULL,
-    email TEXT UNIQUE NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL
-);
