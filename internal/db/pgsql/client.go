8113de05373e8045fe61a4a52228d3d4d16215c7
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2023-07-02 16:53:55 +0700

    refactor(internal/db): Remove auto migration while init ent client

--- internal/db/pgsql/client.go
+++ internal/db/pgsql/client.go
@@ -6,22 +6,12 @@ import (
 
 	"entgo.io/ent/dialect"
 	"github.com/hellohq/hqservice/ent"
-	"github.com/hellohq/hqservice/ent/migrate"
 )
 
 func NewPgClient(ctx context.Context, dateSourceName string) *ent.Client {
 	client, err := ent.Open(dialect.Postgres, dateSourceName)
 	if err != nil {
 		log.Fatalf("failed opening connection to postgres: %v", err)
-	}
-
-	// Run the auto migration tool.
-	if err := client.Schema.Create(ctx,
-		migrate.WithDropIndex(true),
-		migrate.WithDropColumn(true),
-		migrate.WithGlobalUniqueID(true),
-	); err != nil {
-		log.Fatalf("failed creating schema resources: %v", err)
 	}
 
 	return client
--- internal/db/sqlite/client.go
+++ internal/db/sqlite/client.go
@@ -6,7 +6,6 @@ import (
 
 	"entgo.io/ent/dialect"
 	"github.com/hellohq/hqservice/ent"
-	"github.com/hellohq/hqservice/ent/migrate"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -14,15 +13,6 @@ func NewSqliteClient(ctx context.Context, dateSourceName string) *ent.Client {
 	client, err := ent.Open(dialect.SQLite, dateSourceName)
 	if err != nil {
 		log.Fatalf("failed opening connection to sqlite: %v", err)
-	}
-
-	// Run the auto migration tool.
-	if err := client.Schema.Create(ctx,
-		migrate.WithDropIndex(true),
-		migrate.WithDropColumn(true),
-		migrate.WithGlobalUniqueID(true),
-	); err != nil {
-		log.Fatalf("failed creating schema resources: %v", err)
 	}
 
 	return client
