b645aea03e08678bd04e403d9211b3eb8460aaa1
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2023-07-15 13:38:56 +0700

    feat(sqlite): Load cr-sqlite extension

--- internal/db/sqlite/client.go
+++ internal/db/sqlite/client.go
@@ -2,19 +2,38 @@ package sqlite
 
 import (
 	"context"
+	"database/sql"
 	"log"
 
 	"entgo.io/ent/dialect"
+	entsql "entgo.io/ent/dialect/sql"
 	"github.com/hellohq/hqservice/internal/db/sqlite/ent"
 	"github.com/hellohq/hqservice/internal/db/sqlite/ent/migrate"
-	_ "github.com/mattn/go-sqlite3"
+	"github.com/mattn/go-sqlite3"
 )
 
 func NewSqliteClient(dsn string) *ent.Client {
-	client, err := ent.Open(dialect.SQLite, dsn)
+	db, err := sql.Open(dialect.SQLite, dsn)
 	if err != nil {
 		log.Fatalf("failed opening connection to sqlite: %v", err)
 	}
+	conn, err := db.Conn(context.Background())
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+	defer conn.Close()
+	err = conn.Raw(func(driverConn interface{}) error {
+		sqliteConn := driverConn.(*sqlite3.SQLiteConn)
+		sqliteConn.LoadExtension("crsqlite-darwin-aarch64", "sqlite3_crsqlite_init")
+		return nil
+	})
+	drv := entsql.OpenDB(dialect.SQLite, db)
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+
+	client := ent.NewClient(ent.Driver(drv))
+
 	ctx := context.Background()
 	// Run the auto migration tool.
 	if err := client.Schema.Create(ctx,
@@ -23,8 +42,5 @@ func NewSqliteClient(dsn string) *ent.Client {
 	); err != nil {
 		log.Fatalf("failed creating schema resources: %v", err)
 	}
-
-	// client.
-
 	return client
 }
