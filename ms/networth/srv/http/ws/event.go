6658f5eed6e05cc74c7c024cd5c271e9af01da58
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2023-07-31 18:34:10 +0700

    feat(http/ws): Add websocket manager and client to handle ws connection, send/recv mechanism, event sync + ack payloads

---
+++ ms/networth/srv/http/ws/event.go
@@ -0,0 +1,33 @@
+package ws
+
+import "encoding/json"
+
+// Event is the Messages sent over the websocket
+// Used to differ between different actions
+type Event struct {
+	// Type is the message type sent
+	Type string `json:"type"`
+	// Payload is the data Based on the Type
+	Payload json.RawMessage `json:"payload"`
+}
+
+// EventHandler is a function signature that is used to affect messages on the socket and triggered
+// depending on the type
+type EventHandler func(event Event, c *Client) error
+
+// AckPayload is the payload for the "ack" event
+type AckPayload struct {
+	// Message is the message sent
+	Message string `json:"message"`
+}
+
+// #[derive(Clone, Serialize, Deserialize, Debug)]
+// pub struct SyncPayload {
+//     pub site_id: String,
+//     pub change_set: String,
+// }
+
+type SyncPayload struct {
+	SiteId    string `json:"site_id"`
+	ChangeSet string `json:"change_set"`
+}
