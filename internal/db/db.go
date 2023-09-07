f5a7211898cc3b10b32abaa5fb729c17e6309f00
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2023-09-07 18:15:25 +0700

    feat(db.go): Set client cert path

--- internal/db/db.go
+++ internal/db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/hellohq/hqservice/ent"
 	"github.com/hellohq/hqservice/internal/db/pgsql"
@@ -16,7 +17,11 @@ type Db struct {
 
 func InitDbClient(ctxStartupCmdServe context.Context, cfg *sharedconfig.Shared) *Db {
 	cfg.Postgres.SSLMode = pqx.SSLRequire
+	// TODO: Set the following values to env
+	cfg.Postgres.SSLCert = "../configs/pg-pki/client-cert.pem"
+	cfg.Postgres.SSLKey = "../configs/pg-pki/client-key.pem"
 	dateSourceName := cfg.Postgres.FormatDSN()
+	fmt.Println(dateSourceName)
 	pgEnt := pgsql.NewPgEnt(dateSourceName)
 	return &Db{
 		PgEnt: pgEnt,
