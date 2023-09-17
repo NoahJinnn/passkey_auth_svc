392042641d91b1f23048266e31b18b03ad7d74c4
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2023-09-17 18:30:34 +0700

    build(tools.go): Remove tools.go

--- scripts/install/sh/gotools.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-tools=$(go list -f '{{range .Imports}}{{.}} {{end}}' ../tools.go)
-go install `echo $tools`
--- tools.go
+++
@@ -1,8 +0,0 @@
-//go:build tools
-
-package tools
-
-import (
-	_ "github.com/mattn/goveralls"
-	_ "gotest.tools/gotestsum"
-)
